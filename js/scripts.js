1e2bb5a63f383d9f38529c043bd175294d154230
William Kulha <kulha.william@gmail.com>
2018-10-11 10:24:18 -0400

    Add spec so that address book clears input fields on submit and moves the contacts first name to the list of contacts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
+//Business logic
+
+//Constructor for contacts.
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
 }
+
+//User logic
+$(document).ready(function() {
+  $('#new-contact').submit(function(event) {
+    event.preventDefault();
+
+    let contactFirstName = $('#new-first-name').val();
+    let contactLastName = $('#new-last-name').val();
+
+    let newContact = new Contact(contactFirstName, contactLastName);
+
+    $('#contacts').append('<li><span class="contact">' + newContact.firstName + '<span></li>');
+    $('#new-first-name').val("");
+    $('#new-last-name').val("");
+  });
+});
