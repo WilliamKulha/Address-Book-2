3f88274ba63d17b3ab978252b089dfad4bed2e1d
John Kulha <kulha.william@gmail.com>
2018-10-13 07:44:55 -0400

    Create scripts to gather each new address and push to the addresses array of the new contact.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 //Business logic
 
+//Put Address form here so it's less cumbersome in the main code.
+const addressForm = '<div class="new-address">' +
+                       '<div class="form-group">' +
+                         '<label for="new-street">Street</label>' +
+                         '<input type="text" class="form-control new-street">' +
+                       '</div>' +
+                       '<div class="form-group">' +
+                         '<label for="new-city">City</label>' +
+                         '<input type="text" class="form-control new-city">' +
+                       '</div>' +
+                       '<div class="form-group">' +
+                         '<label for="new-state">State</label>' +
+                         '<input type="text" class="form-control new-state">' +
+                       '</div>' +
+                     '</div>';
+
 //Constructor for contacts.
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
-  this.address = [];
+  this.addresses = [];
 }
 
+//Constructor for addresses.
 function Address(street, city, state, zip) {
   this.street = street;
   this.city = city;
@@ -18,8 +35,17 @@ function Address(street, city, state, zip) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+//New method for address prototype to return full addresses.
+Address.prototype.fullAddress = function() {
+  return `${this.street} <br> ${this.city}, ${this.state}<br>${this.zip}`
+}
 //User logic
 $(document).ready(function() {
+  $('#add-address').click(function() {
+    $('#new-addresses').append(addressForm);
+  });
+
   $('#new-contact').submit(function(event) {
     event.preventDefault();
 
@@ -28,6 +54,14 @@ $(document).ready(function() {
 
     let newContact = new Contact(contactFirstName, contactLastName);
 
+    $('.new-address').each(function() {
+      let inputtedStreet = $(this).find(`input.new-street`).val();
+      let inputtedCity = $(this).find(`input.new-city`).val();
+      let inputtedState = $(this).find(`input.new-state`).val();
+      let inputtedZip = $(this).find(`input.new-zip`).val();
+      let newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip);
+      newContact.addresses.push(newAddress);
+    });
     $('#contacts').append('<li><span class="contact">' + newContact.fullName() + '<span></li>');
     $('.contact').last().click(function() {
       $('#show-contact').slideDown();
